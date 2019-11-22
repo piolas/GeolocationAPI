ea84e5fa4f55b9788b8887780c990c0f2aec9091
Piotr Łaskawski <piotr.laskawski@outlook.com>
2019-11-22 13:13:47 +0100

    Libraries to store Infrastructure and Domain dependencies

---
+++ GeolocationAPI/Geolocation.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Geolocation.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ GeolocationAPI/Geolocation.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Geolocation.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
