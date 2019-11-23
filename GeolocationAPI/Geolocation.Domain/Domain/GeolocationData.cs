5e8bd1a775c750d75d772444372e7a8f511ff9e3
Piotr Łaskawski <piotr.laskawski@outlook.com>
2019-11-23 10:28:33 +0100

    Initial DTO's to pass URL/IP parameter and entity to store data from external service

---
+++ GeolocationAPI/Geolocation.Domain/Domain/GeolocationData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geolocation.Domain.Domain
+{
+    public sealed class GeolocationData
+    {
+
+    }
+}
---
+++ GeolocationAPI/Geolocation.Infrastructure/DTO/IPDataDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Geolocation.Infrastructure.DTO
+{
+    public sealed class IPDataDTO
+    {
+        public string IPParameter { get; set; }
+    }
+}
