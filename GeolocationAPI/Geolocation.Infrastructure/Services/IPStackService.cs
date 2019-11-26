39f0adcc0b4cd23fccf25288c32df4d92a244e00
Piotr Łaskawski <piotr.laskawski@outlook.com>
2019-11-26 20:07:19 +0100

    Initial implementation of calling IPStack service

--- GeolocationAPI/Geolocation.Infrastructure/Services/IPStackService.cs
+++ GeolocationAPI/Geolocation.Infrastructure/Services/IPStackService.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Geolocation.Infrastructure.DTO;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Geolocation.Infrastructure.Services
 {
@@ -17,8 +20,20 @@ namespace Geolocation.Infrastructure.Services
         private string GetIPStackAPIKey()
         {
             string vaultName = _configuration["IPStackAPIKey"];           
+            return vaultName;
+        }
 
-            return "";
+        public async Task<GeolocationResponseDTO> GetDataFromRemoteAPI(string paramter)
+        {
+            var apikey = GetIPStackAPIKey();
+
+            var client = new RestClient("http://api.ipstack.com");
+
+            var request = new RestRequest($"{paramter}/?access_key={apikey}");
+
+            var response = await client.ExecuteTaskAsync<GeolocationResponseDTO>(request);           
+
+            return response.Data;
         }
     }
 }
