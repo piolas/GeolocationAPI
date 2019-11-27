14c4ce2b6b16a5326dbfdddfae2b7cae26b3779e
Piotr Łaskawski <piotr.laskawski@outlook.com>
2019-11-27 21:16:03 +0100

    Retry policy for calling IPStack service

--- GeolocationAPI/Geolocation.Infrastructure/Services/IPStackService.cs
+++ GeolocationAPI/Geolocation.Infrastructure/Services/IPStackService.cs
@@ -1,6 +1,9 @@
 ﻿using Geolocation.Infrastructure.DTO;
 using Microsoft.Extensions.Configuration;
+using Polly;
 using RestSharp;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Geolocation.Infrastructure.Services
@@ -22,13 +25,26 @@ namespace Geolocation.Infrastructure.Services
 
         public async Task<GeolocationResponseDTO> GetDataFromRemoteAPI(string paramter)
         {
+            var maxRetryAttempts = 3;
+            var pauseBetweenFailures = TimeSpan.FromSeconds(2);
+
+            var retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(maxRetryAttempts, i => pauseBetweenFailures);
+
+
             var apikey = GetIPStackAPIKey();
 
             var client = new RestClient("http://api.ipstack.com");
 
             var request = new RestRequest($"{paramter}?access_key={apikey}");
 
-            var response = await client.ExecuteTaskAsync<GeolocationResponseDTO>(request);           
+            IRestResponse<GeolocationResponseDTO> response = null;
+
+            await retryPolicy.ExecuteAsync(async () =>
+            {
+                response = await client.ExecuteTaskAsync<GeolocationResponseDTO>(request);                
+            });
 
             return response.Data;
         }
